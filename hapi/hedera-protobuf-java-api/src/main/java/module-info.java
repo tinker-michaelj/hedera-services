bb014388a72daf292c708f2ffe5052b06f22d90d
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-02-26 07:50:07 +0100

    build: publish 'hedera-protobuf-java-api' from services repo (#17737)
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

---
+++ hapi/hedera-protobuf-java-api/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0
+module com.hedera.protobuf.java.api {
+    exports com.hedera.hapi.block.protoc;
+    exports com.hedera.hapi.block.stream.input.protoc;
+    exports com.hedera.hapi.block.stream.output.protoc;
+    exports com.hedera.hapi.block.stream.protoc;
+    exports com.hedera.hapi.node.state.tss.legacy;
+    exports com.hedera.hapi.platform.event.legacy;
+    exports com.hedera.hapi.platform.state.legacy;
+    exports com.hedera.hapi.services.auxiliary.hints.legacy;
+    exports com.hedera.hapi.services.auxiliary.history.legacy;
+    exports com.hedera.hapi.services.auxiliary.tss.legacy;
+    exports com.hedera.services.stream.proto;
+    exports com.hederahashgraph.api.proto.java;
+    exports com.hederahashgraph.service.proto.java;
+
+    requires transitive com.google.common;
+    requires transitive com.google.protobuf;
+    requires transitive io.grpc.stub;
+    requires transitive io.grpc;
+    requires io.grpc.protobuf;
+}
