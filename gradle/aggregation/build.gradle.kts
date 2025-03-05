812c846d85ff1ede8f2624133e46ecedee0ab904
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-03-05 18:52:02 +0100

    ci: fix and cleanup snyk/gradle integration (#18202)
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 plugins {
     id("org.hiero.gradle.base.lifecycle")
+    id("org.hiero.gradle.base.version")
     id("org.hiero.gradle.report.code-coverage")
     id("org.hiero.gradle.check.spotless")
     id("org.hiero.gradle.check.spotless-kotlin")
