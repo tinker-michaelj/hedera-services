16667fa269bd87f0141ba18979a868382b61d675
Neeharika Sompalli <52669918+Neeharika-Sompalli@users.noreply.github.com>
2025-04-14 17:09:17 -0500

    fix: Fix flakiness in HIP-1064 tests (#18822)
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/integration/RepeatableHip1064Tests.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/integration/RepeatableHip1064Tests.java
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 package com.hedera.services.bdd.suites.integration;
 
+import static com.hedera.hapi.util.HapiUtils.asInstant;
+import static com.hedera.node.app.hapi.utils.CommonPbjConverters.toPbj;
 import static com.hedera.services.bdd.junit.RepeatableReason.NEEDS_VIRTUAL_TIME_FOR_FAST_EXECUTION;
 import static com.hedera.services.bdd.junit.TestTags.INTEGRATION;
 import static com.hedera.services.bdd.junit.hedera.embedded.EmbeddedMode.REPEATABLE;
@@ -48,9 +50,11 @@ import com.hedera.services.bdd.spec.utilops.streams.assertions.VisibleItemsValid
 import com.hederahashgraph.api.proto.java.AccountAmount;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.LongSupplier;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeAll;
@@ -98,18 +102,20 @@ public class RepeatableHip1064Tests {
     final Stream<DynamicTest> paysAdjustedFeesToAllEligibleActiveAccountsAtStartOfNewPeriod() {
         final AtomicLong expectedNodeFees = new AtomicLong(0);
         final AtomicLong expectedNodeRewards = new AtomicLong(0);
+        final AtomicReference<Instant> startConsensusTime = new AtomicReference<>();
         return hapiTest(
-                recordStreamMustIncludePassFrom(
-                        selectedItems(
-                                nodeRewardsValidator(expectedNodeRewards::get),
-                                // We expect two node rewards payments in this test.
-                                // But first staking period all nodes are inactive and minReward is 0.
-                                // So no synthetic node rewards payment is expected.
-                                1,
-                                (spec, item) -> item.getRecord().getTransferList().getAccountAmountsList().stream()
-                                        .anyMatch(aa ->
-                                                aa.getAccountID().getAccountNum() == 801L && aa.getAmount() < 0L)),
-                        Duration.ofSeconds(1)),
+                doingContextual(spec -> startConsensusTime.set(spec.consensusTime())),
+                recordStreamMustIncludePassFrom(selectedItems(
+                        nodeRewardsValidator(expectedNodeRewards::get),
+                        // We expect two node rewards payments in this test.
+                        // But first staking period all nodes are inactive and minReward is 0.
+                        // So no synthetic node rewards payment is expected.
+                        1,
+                        (spec, item) -> item.getRecord().getTransferList().getAccountAmountsList().stream()
+                                        .anyMatch(
+                                                aa -> aa.getAccountID().getAccountNum() == 801L && aa.getAmount() < 0L)
+                                && asInstant(toPbj(item.getRecord().getConsensusTimestamp()))
+                                        .isAfter(startConsensusTime.get()))),
                 cryptoTransfer(TokenMovement.movingHbar(100000 * ONE_HBAR).between(GENESIS, NODE_REWARD)),
                 nodeUpdate("0").declineReward(true),
                 // Start a new period
@@ -121,12 +127,13 @@ public class RepeatableHip1064Tests {
                         .payingWith(CIVILIAN_PAYER)
                         .via("notFree"),
                 // Collects ~1.8M tinybar in node fees; so ~450k tinybar per node
-                getTxnRecord("notFree")
-                        .exposingTo(r -> expectedNodeFees.set(r.getTransferList().getAccountAmountsList().stream()
-                                .filter(a -> a.getAccountID().getAccountNum() == 3L)
-                                .findFirst()
-                                .orElseThrow()
-                                .getAmount())),
+                getTxnRecord("notFree").exposingTo(r -> {
+                    expectedNodeFees.set(r.getTransferList().getAccountAmountsList().stream()
+                            .filter(a -> a.getAccountID().getAccountNum() == 3L)
+                            .findFirst()
+                            .orElseThrow()
+                            .getAmount());
+                }),
                 // validate all network fees go to 0.0.801
                 validateRecordFees("notFree", List.of(3L, 801L)),
                 doWithStartupConfig(
@@ -185,8 +192,10 @@ public class RepeatableHip1064Tests {
     final Stream<DynamicTest> paysNonAdjustedFeesToAllEligibleActiveAccountsAtStartOfNewPeriod() {
         final AtomicLong expectedNodeFees = new AtomicLong(0);
         final AtomicLong expectedNodeRewards = new AtomicLong(0);
+        final AtomicReference<Instant> startConsensusTime = new AtomicReference<>();
         return hapiTest(
                 overriding("nodes.adjustNodeFees", "false"),
+                doingContextual(spec -> startConsensusTime.set(spec.consensusTime())),
                 recordStreamMustIncludePassFrom(
                         selectedItems(
                                 nodeRewardsValidator(expectedNodeRewards::get),
@@ -195,8 +204,11 @@ public class RepeatableHip1064Tests {
                                 // So no synthetic node rewards payment is expected.
                                 1,
                                 (spec, item) -> item.getRecord().getTransferList().getAccountAmountsList().stream()
-                                        .anyMatch(aa ->
-                                                aa.getAccountID().getAccountNum() == 801L && aa.getAmount() < 0L)),
+                                                .anyMatch(
+                                                        aa -> aa.getAccountID().getAccountNum() == 801L
+                                                                && aa.getAmount() < 0L)
+                                        && asInstant(toPbj(item.getRecord().getConsensusTimestamp()))
+                                                .isAfter(startConsensusTime.get())),
                         Duration.ofSeconds(1)),
                 cryptoTransfer(TokenMovement.movingHbar(100000 * ONE_HBAR).between(GENESIS, NODE_REWARD)),
                 nodeUpdate("0").declineReward(true),
@@ -271,11 +283,13 @@ public class RepeatableHip1064Tests {
             overrides = {"nodes.minPerPeriodNodeRewardUsd"})
     @Order(2)
     final Stream<DynamicTest> inactiveNodesPaidWhenMinRewardsGreaterThanZero() {
+        final AtomicReference<Instant> startConsensusTime = new AtomicReference<>();
         final AtomicLong expectedNodeFees = new AtomicLong(0);
         final AtomicLong expectedNodeRewards = new AtomicLong(0);
         final AtomicLong expectedMinNodeReward = new AtomicLong(0);
         return hapiTest(
                 overriding("nodes.minPerPeriodNodeRewardUsd", "10"),
+                doingContextual(spec -> startConsensusTime.set(spec.consensusTime())),
                 recordStreamMustIncludePassFrom(
                         selectedItems(
                                 nodeRewardsValidatorWithInactiveNodes(
@@ -285,8 +299,11 @@ public class RepeatableHip1064Tests {
                                 // Second staking period, two nodes are active and one node is inactive
                                 2,
                                 (spec, item) -> item.getRecord().getTransferList().getAccountAmountsList().stream()
-                                        .anyMatch(aa ->
-                                                aa.getAccountID().getAccountNum() == 801L && aa.getAmount() < 0L)),
+                                                .anyMatch(
+                                                        aa -> aa.getAccountID().getAccountNum() == 801L
+                                                                && aa.getAmount() < 0L)
+                                        && asInstant(toPbj(item.getRecord().getConsensusTimestamp()))
+                                                .isAfter(startConsensusTime.get())),
                         Duration.ofSeconds(1)),
                 nodeUpdate("0").declineReward(true),
                 cryptoTransfer(TokenMovement.movingHbar(10000000 * ONE_HBAR).between(GENESIS, NODE_REWARD)),
@@ -349,10 +366,12 @@ public class RepeatableHip1064Tests {
             overrides = {"nodes.preserveMinNodeRewardBalance"})
     @Order(3)
     final Stream<DynamicTest> preserveNodeRewardBalanceHasEffectWhenFeatureEnabled() {
+        final AtomicReference<Instant> startConsensusTime = new AtomicReference<>();
         final AtomicLong expectedNodeFees = new AtomicLong(0);
         final AtomicLong expectedNodeRewards = new AtomicLong(0);
         return hapiTest(
                 overriding("nodes.preserveMinNodeRewardBalance", "false"),
+                doingContextual(spec -> startConsensusTime.set(spec.consensusTime())),
                 recordStreamMustIncludePassFrom(
                         selectedItems(
                                 nodeRewardsValidator(expectedNodeRewards::get),
@@ -361,8 +380,11 @@ public class RepeatableHip1064Tests {
                                 // So no synthetic node rewards payment is expected.
                                 1,
                                 (spec, item) -> item.getRecord().getTransferList().getAccountAmountsList().stream()
-                                        .anyMatch(aa ->
-                                                aa.getAccountID().getAccountNum() == 801L && aa.getAmount() < 0L)),
+                                                .anyMatch(
+                                                        aa -> aa.getAccountID().getAccountNum() == 801L
+                                                                && aa.getAmount() < 0L)
+                                        && asInstant(toPbj(item.getRecord().getConsensusTimestamp()))
+                                                .isAfter(startConsensusTime.get())),
                         Duration.ofSeconds(1)),
                 cryptoTransfer(TokenMovement.movingHbar(100000 * ONE_HBAR).between(GENESIS, NODE_REWARD)),
                 nodeUpdate("0").declineReward(true),
