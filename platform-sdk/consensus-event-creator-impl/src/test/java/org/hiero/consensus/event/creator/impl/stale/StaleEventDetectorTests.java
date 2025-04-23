2f03a67cca977c7c03470a7ad84f8762ac702a31
Michael Heinrichs <netopyr@users.noreply.github.com>
2025-04-23 16:22:05 +0200

    chore: Moved StaleEventDetectorTests to event-creator module (#18971)
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- platform-sdk/consensus-event-creator-impl/src/test/java/org/hiero/consensus/event/creator/impl/stale/StaleEventDetectorTests.java
+++ platform-sdk/consensus-event-creator-impl/src/test/java/org/hiero/consensus/event/creator/impl/stale/StaleEventDetectorTests.java
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-package com.swirlds.platform.event.stale;
+package org.hiero.consensus.event.creator.impl.stale;
 
 import static org.hiero.consensus.model.event.AncientMode.BIRTH_ROUND_THRESHOLD;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,8 +21,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import org.hiero.consensus.config.EventConfig_;
-import org.hiero.consensus.event.creator.impl.stale.DefaultStaleEventDetector;
-import org.hiero.consensus.event.creator.impl.stale.StaleEventDetector;
 import org.hiero.consensus.model.event.PlatformEvent;
 import org.hiero.consensus.model.event.StaleEventDetectorOutput;
 import org.hiero.consensus.model.hashgraph.ConsensusRound;
